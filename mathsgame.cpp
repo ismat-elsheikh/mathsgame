2cdf7ca43fccec1310912af582b318e17d3029b1
Rahaf Elsheikh <ismat1999@gmail.com>
2020-12-19 11:08:56 +0800

    third commit

--- mathsgame.cpp
+++ mathsgame.cpp
@@ -5,4 +5,5 @@ int main ()
     cout<< "Hello World!"<<endl;
     cout<< "Hello World! Its ME, Again....."<<endl;
     cout<<"Is this google docs but for code?"<<endl;
+    cout<<"I think it actually is google docs but for code, with some unix commands for whatever reason"<<endl;
 }
